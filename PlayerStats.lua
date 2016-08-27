8ba6b34d58aff077f752afa76eac91f710f85a33
Adam Proctor <aproctor@gmail.com>
2016-08-26 23:27:52 -0400

    twerks

--- PlayerStats.lua
+++ PlayerStats.lua
@@ -1,16 +1,13 @@
 --- Player Stats Addon
 function PlayerStats()
 	print("Initializing Player Stats");
-	RequestTimePlayed();
 end
 
 local pst_frame = CreateFrame("Frame");
 pst_frame:RegisterEvent("TIME_PLAYED_MSG");
 
-pst_frame:SetScript("OnEvent",
-function(self,event,...) 
-
-	 if event == "TIME_PLAYED_MSG" then
+pst_frame:SetScript("OnEvent", function(self,event,...) 
+	if event == "TIME_PLAYED_MSG" then
 		print("Your play time on this character is <" ..  select(1, ...) .. "> seconds");
 	end
 end);
