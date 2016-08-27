877cc4e2942a99ecbed34421c9b1ba52382aa9dd
Autris <daniel.wagnerjr@gmail.com>
2016-08-26 23:23:00 -0400

    Update PlayerStats.lua

--- PlayerStats.lua
+++ PlayerStats.lua
@@ -1,5 +1,8 @@
 --- Player Stats Addon
+local InScript = false;
+
 function PlayerStats()
+	InScript = true;
 	print("Initializing Player Stats");
 	RequestTimePlayed();
 end
@@ -10,7 +13,7 @@ pst_frame:RegisterEvent("TIME_PLAYED_MSG");
 pst_frame:SetScript("OnEvent",
 function(self,event,...) 
 
-	 if event == "TIME_PLAYED_MSG" then
+	 if event == "TIME_PLAYED_MSG" and (InScript) then
 		print("Your play time on this character is <" ..  select(1, ...) .. "> seconds");
 	end
 end);
