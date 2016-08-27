f7914e5a09abfb7e46170bd3f24b3ea561570fd0
Adam Proctor <aproctor@gmail.com>
2016-08-26 22:59:58 -0400

    basic addon commit

---
+++ PlayerStats.lua
@@ -0,0 +1,16 @@
+--- Player Stats Addon
+function PlayerStats()
+	print("Initializing Player Stats");
+	RequestTimePlayed();
+end
+
+local pst_frame = CreateFrame("Frame");
+pst_frame:RegisterEvent("TIME_PLAYED_MSG");
+
+pst_frame:SetScript("OnEvent",
+function(self,event,...) 
+
+	 if event == "TIME_PLAYED_MSG" then
+		print("Your play time on this character is <" ..  select(1, ...) .. "> seconds");
+	end
+end);
---
+++ README.md
@@ -0,0 +1,3 @@
+# Player Stats Addon
+
+WIP
